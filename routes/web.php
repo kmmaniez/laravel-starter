effb245478eeba2c9c2e044d60c2b8da15489e1b
kmmaniez <dzlabsid@gmail.com>
2023-03-10 21:29:21 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin.dashboard');
 });
+
+Route::resource('products', ProductController::class)
+            ->only(['index','create','store','edit','update','destroy']);
