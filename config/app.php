c97d793607e07bbe474c7e480b14400dfaa912f5
kmmaniez <dzlabsid@gmail.com>
2023-03-11 23:44:12 +0700

    Update App config

--- config/app.php
+++ config/app.php
@@ -186,6 +186,8 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -211,6 +213,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+        'PDF'   => Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];
