0f06398a7cf882211e065bdf3d6855fffdbe9399
kmmaniez <dzlabsid@gmail.com>
2023-03-12 19:08:06 +0700

    Update product index

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -6,17 +6,14 @@
     
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800">{{ $title_page }} Page</h1>
-        
-        {{-- @if(auth()->user()->is_admin) --}}
-        {{-- @endif --}}
 
-        <!-- DataTales -->
+        <!-- Card -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">List {{ $title_page }}</h6>
             </div>
             <div class="card-body">
-                <a href="/products/create" class="btn btn-md btn-primary mb-4"><i class="fas fa-fw fa-user-plus"></i> Add {{ $title_page }}</a>  
+                <a href="javascript:void(0)" class="btn btn-md btn-primary mb-4" id="create-post"><i class="fas fa-fw fa-user-plus"></i> Add {{ $title_page }}</a>  
         
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -29,23 +26,21 @@
                                 <th>Action</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            @foreach ($product as $data)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>{{ $data->name }}</td>
-                                <td>{{ $data->quantity }}</td>
-                                <td>{{ $data->price }}</td>
-                                <td>
-                                    <form action="{{ route('products.store') }}/{{ $data->id }}" method="post">
-                                        @csrf
-                                        @method('delete')
-                                        <a class="btn btn-sm btn-primary" href="{{ route('products.edit', $data) }}">Edit</a>
-                                        <a class="btn btn-sm btn-danger delete" id="{{ $data->id }}">Delete</a>
-                                    </form>
-                                </td>
-                            </tr>
-                            @endforeach
+                        <tbody> 
+                        @foreach ($product as $data)<tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $data->name }}</td>
+                            <td>{{ $data->quantity }}</td>
+                            <td>{{ $data->price }}</td>
+                            <td>
+                                <a class="btn btn-sm btn-primary" href="{{ route('products.edit', $data) }}">Edit</a>
+                                <form action="{{ route('products.store') }}/{{ $data->id }}" method="post">
+                                    @csrf 
+                                    @method('delete')<a class="btn btn-sm btn-danger delete" id="{{ $data->id }}">Delete</a>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -53,5 +48,4 @@
         </div>
         
     </div>
-
 @endsection
