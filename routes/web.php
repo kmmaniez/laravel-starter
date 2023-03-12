239d248de8ae7d06d0a0449842aedf2b0ea76490
kmmaniez <dzlabsid@gmail.com>
2023-03-12 10:43:26 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Blog\CategoryController;
 use App\Http\Controllers\Blog\PostController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,17 +12,27 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    // return view('auth.login');
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/admin', function () {
     return view('admin.dashboard');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::resource('products', ProductController::class)->only(['index','create','store','edit','update','destroy']);
@@ -35,4 +46,6 @@ Route::prefix('blog')->group(function () {
     Route::resource('post', PostController::class)->only(['index','create','store','edit','update','destroy']);
     Route::resource('category', CategoryController::class)->only(['index','create','store','edit','update','destroy']);
     
-});
+})->middleware(['guest']);
+
+require __DIR__.'/auth.php';
