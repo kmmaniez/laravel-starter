6593830b19ff839d931274b7d6a5b4b216e213ef
kmmaniez <dzlabsid@gmail.com>
2023-03-13 12:13:36 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,8 @@ Route::get('/admin', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
