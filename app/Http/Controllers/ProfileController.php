1f69bff92cb58f56e965c8515b18b8d5e8bf5e8f
kmmaniez <dzlabsid@gmail.com>
2023-03-13 15:57:52 +0700

    Update ProfileController on function update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,15 +11,14 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    public function index()
+
+    public function index(): View
     {
         return view('profile.index', [
             'title_page'    => 'Data Products'
         ]);
     }
-    /**
-     * Display the user's profile form.
-     */
+    
      public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -27,9 +26,6 @@ class ProfileController extends Controller
         ]);
     }
 
-    /**
-     * Update the user's profile information.
-     */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
@@ -39,13 +35,9 @@ class ProfileController extends Controller
         }
 
         $request->user()->save();
-
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.index')->with('status', 'profile-updated');
     }
 
-    /**
-     * Delete the user's account.
-     */
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
