8a86c5bf7893f0f83e29f0318e01475c79cec3db
kmmaniez <dzlabsid@gmail.com>
2023-03-13 12:14:25 +0700

    Add index function ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,10 +11,16 @@ use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
+    public function index()
+    {
+        return view('profile.index', [
+            'title_page'    => 'Data Products'
+        ]);
+    }
     /**
      * Display the user's profile form.
      */
-    public function edit(Request $request): View
+     public function edit(Request $request): View
     {
         return view('profile.edit', [
             'user' => $request->user(),
