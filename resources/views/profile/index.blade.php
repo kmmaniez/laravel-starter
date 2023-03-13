4e028f9b49e819fda4422c6fc5832121f2b39499
kmmaniez <dzlabsid@gmail.com>
2023-03-13 15:58:43 +0700

    Add Profile index page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -8,7 +8,7 @@
 
             <div class="col-lg-3">
     
-                <!-- Basic Card Example -->
+                <!-- Photo -->
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
                         <h6 class="m-0 font-weight-bold text-primary">Photo Profile</h6>
@@ -30,8 +30,14 @@
                     </a>
                     <div class="collapse show" id="profileinfo">
                         <div class="card-body">
+                            
+                            @if (session('status') === 'profile-updated')
+                                <div class="alert alert-success alert-notif"><i class="fas fa-fw fa-check"></i> <strong>Profile settings saved</strong></div>
+                            @endif
+                            
                             <form action="{{ route('profile.update') }}" method="post">
                                 @csrf
+                                @method('patch')
                                 <div class="form-group">
                                     <label for="name">Name</label>
                                     <input type="text" name="name" id="name" class="form-control" value="{{ Auth::user()->name }}">
@@ -46,6 +52,12 @@
                                     <label for="address">Address</label>
                                     <textarea type="text" name="address" id="address" class="form-control" rows="3">{{ Auth::user()->created_at }}</textarea>
                                 </div>
+                                
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <input type="file" name="" id="" class="form-control">
+                                </div>
+
                                 <button class="btn btn-md btn-primary">Update profile</button>
                             </form>
                         </div>
@@ -55,27 +67,29 @@
 
                 <!-- Change password -->
                 <div class="card shadow mb-4">
-                    <!-- Card Header - Accordion -->
                     <a href="#changepassword" class="d-block card-header py-3" data-toggle="collapse"
                         role="button" aria-expanded="true" aria-controls="changepassword">
                         <h6 class="m-0 font-weight-bold text-primary">Security</h6>
                     </a>
-                    <!-- Card Content - Collapse -->
-                    <div class="collapse" id="changepassword">
+                    <div class="collapse show" id="changepassword">
                         <div class="card-body">
-                            <form action="{{ route('profile.update') }}" method="post">
+                            @if (session('status') === 'password-updated')
+                                <div class="alert alert-success alert-notif"><i class="fas fa-fw fa-check"></i> <strong>Profile settings saved</strong></div>
+                            @endif
+                            <form action="{{ route('password.update') }}" method="post">
                                 @csrf
+                                @method('put')
                                 <div class="form-group">
                                     <label for="current_password">Current Password</label>
-                                    <input type="text" name="current_password" id="current_password" class="form-control" value="{{ Auth::user()->name }}">
+                                    <input type="password" name="current_password" id="current_password" class="form-control">
                                 </div>
                                 <div class="form-group">
                                     <label for="password">New Password</label>
-                                    <input type="text" name="password" id="password" class="form-control" value="{{ Auth::user()->email }}">
+                                    <input type="password" name="password" id="password" class="form-control">
                                 </div>
                                 <div class="form-group">
                                     <label for="password_confirmation">Confirm Password</label>
-                                    <input type="text" name="password_confirmation" id="password_confirmation" class="form-control" value="{{ Auth::user()->email }}">
+                                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
                                 </div>
                                 <button class="btn btn-md btn-primary">Update password</button>
                             </form>
