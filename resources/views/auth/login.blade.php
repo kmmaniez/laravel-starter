6f44322ad8927d9ba9580e88626b29570bb72a86
kmmaniez <dzlabsid@gmail.com>
2023-03-14 21:20:51 +0700

    Update login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,23 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex">
+            <x-primary-button class="w-full justify-center mt-4">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+
+        <!-- Don't have account & forgot  -->
+        <div class="flex justify-between mt-8">
+            <a class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                {{ __("Don't have an account?") }}
+            </a>
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
         </div>
+
     </form>
 </x-guest-layout>
