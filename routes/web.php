ad616d5bec7b5b7ef4f6093e0c15c8da6e411ed9
kmmaniez <dzlabsid@gmail.com>
2023-03-14 21:18:16 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -17,36 +17,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
-    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+
+    // Route Dashboard
+    Route::view('/', 'admin.dashboard')->name('dashboard');
+
+    // Route Profile
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.index');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route Products
+    Route::resource('products', ProductController::class)->only(['index','create','store','edit','update','destroy']);
+    
+    // Route Blog (prefix) => localhost/blog/*
+    Route::prefix('blog')->group(function () {
+
+        // Default Route Blog prefix
+        Route::view('/','admin.dashboard');
+
+        // Route Post
+        Route::resource('post', PostController::class)->only(['index','create','store','edit','update','destroy']);
+        
+        // Route Category
+        Route::resource('category', CategoryController::class)->only(['index','create','store','edit','update','destroy']);
+    });
 });
 
-Route::resource('products', ProductController::class)->only(['index','create','store','edit','update','destroy']);
 
-Route::prefix('blog')->group(function () {
-    
-    Route::get('/', function () {
-        return view('admin.dashboard');
-    });
-
-    Route::resource('post', PostController::class)->only(['index','create','store','edit','update','destroy']);
-    Route::resource('category', CategoryController::class)->only(['index','create','store','edit','update','destroy']);
-    
-})->middleware(['guest']);
 
 require __DIR__.'/auth.php';
