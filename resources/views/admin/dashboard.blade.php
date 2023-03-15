5ab7e8c607538bb538b2eb585a197a937e1ea898
kmmaniez <dzlabsid@gmail.com>
2023-03-15 21:47:18 +0700

    Update dashboard admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,21 +6,21 @@
 
             <!-- Page Heading -->
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Dashboard Admin</h1>
+                <h1 class="h3 mb-0 text-gray-800">{{ Auth::user()->name }}</h1>
             </div>
 
             <!-- Content Row -->
             <div class="row">
                 
-                <!-- Total barang -->
+                <!-- Categories -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-bottom-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Total Barang</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                                        Categories</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $categories_count }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-list fa-2x text-gray-300"></i>
@@ -30,15 +30,15 @@
                     </div>
                 </div>
 
-                <!-- Total barang masuk -->
+                <!-- Posts -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-bottom-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Total Barang Masuk</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">1</div>
+                                        Posts</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $posts_count }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -48,15 +48,15 @@
                     </div>
                 </div>
                 
-                <!-- Total barang keluar -->
+                <!-- Products -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-bottom-success shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Total Barang Keluar</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                                        Products</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $products_count }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -66,18 +66,18 @@
                     </div>
                 </div>
                 
-                <!-- Total laporan -->
+                <!-- User -->
                 <div class="col-xl-3 col-md-6 mb-4">
                     <div class="card border-bottom-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                        Total Laporan</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">3</div>
+                                        User</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $users_count }}</div>
                                 </div>
                                 <div class="col-auto">
-                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                                    <i class="fas fa-user fa-2x text-gray-300"></i>
                                 </div>
                             </div>
                         </div>
