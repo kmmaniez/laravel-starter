eb484254394ecb754519e385f5f5f736c58698b6
kmmaniez <dzlabsid@gmail.com>
2023-03-15 12:18:34 +0700

    Update create modal components

--- resources/views/components/admin/create-modal.blade.php
+++ resources/views/components/admin/create-modal.blade.php
@@ -1,33 +1,23 @@
         <!-- Modal -->
         <div class="modal fade" id="modal-create" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
+            <div class="modal-dialog modal-md">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Add Categories</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
                         <div class="form-group">
-                            <label for="name" class="control-label">Name</label>
-                            <input type="text" class="form-control" id="name">
-                            <div class="alert alert-danger mt-2 d-none" role="alert" id="alert-name"></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="price" class="control-label">Price</label>
-                            <input type="text" class="form-control" id="price">
-                            <div class="alert alert-danger mt-2 d-none" role="alert" id="alert-price"></div>
-                        </div>
-                        <div class="form-group">
-                            <label for="quantity" class="control-label">Quantity</label>
-                            <input type="number" min="1" class="form-control" id="quantity">
-                            <div class="alert alert-danger mt-2 d-none" role="alert" id="alert-quantity"></div>
+                            <label for="category_name" class="control-label">Category Name</label>
+                            <input type="text" class="form-control" id="category_name" name="category_name" required autofocus>
+                            <div class="alert alert-danger mt-2 d-none" role="alert" id="alert_category"></div>
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <button type="button" class="btn btn-primary" id="save">Save Data</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     </div>
                 </div>
             </div>
