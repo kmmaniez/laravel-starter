5fd1cd80cbfdf3ed14899c39201de6ad855eef11
kmmaniez <dzlabsid@gmail.com>
2023-03-15 21:47:37 +0700

    Update create-modal

--- resources/views/components/admin/create-modal.blade.php
+++ resources/views/components/admin/create-modal.blade.php
@@ -1,9 +1,9 @@
         <!-- Modal -->
-        <div class="modal fade" id="modal-create" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="modal-create" tabindex="-1" aria-labelledby="modal-create" aria-hidden="true">
             <div class="modal-dialog modal-md">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add Categories</h5>
+                        <h5 class="modal-title" id="modal-create">Add Categories</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
