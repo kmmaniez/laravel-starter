6ae27bf6138694aaf24aaa026f97395eca693963
kmmaniez <dzlabsid@gmail.com>
2023-03-18 22:01:51 +0700

    Update Post model add relationship with Category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,11 @@ class Post extends Model
     use HasFactory, Sluggable;
     protected $guarded = ['id'];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function getRouteKeyName()
     {
         return 'slug';
