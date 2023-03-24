075a2441ee9a7eab04196543490e9066f22ac016
kmmaniez <dzlabsid@gmail.com>
2023-03-24 16:54:53 +0700

    Update index function to configure datatable

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,16 +7,40 @@ use Illuminate\Http\Request;
 use Alert;
 use App\Http\Requests\StoreProductRequest;
 use Spatie\Permission\Models\Role;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        return view('admin.products.index', [
-            'title_page'    => 'Data Products',
-            'product'       => Product::all()
-        ]);
+        if (request()->ajax()) {
+            $users = Product::query();
+            return DataTables::eloquent($users)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+                        $actionBtn = '<a href="#" id="edit-post" class="btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                        return $actionBtn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+        return view('admin.products.index', ['title_page'    => 'Data Products']);
+    }
+    public function indexs(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = User::select('id','name','email')->get();
+            return DataTables::of($data)->addIndexColumn()
+                ->addColumn('action', function($row){
+                    $btn = '<a href="javascript:void(0)" class="btn btn-primary btn-sm">View</a>';
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('users');
     }
 
     public function create()
