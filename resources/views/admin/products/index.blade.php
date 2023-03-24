9f800186e9fd4dfc9c72c76ad6b455725444b8e2
kmmaniez <dzlabsid@gmail.com>
2023-03-24 16:56:05 +0700

    Add id to table

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -16,7 +16,7 @@
                 <a href="{{ route('products.create') }}" class="btn btn-md btn-primary mb-4" id="create-post"><i class="fas fa-fw fa-user-plus"></i> Add {{ $title_page }}</a>  
         
                 <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered" id="productDataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr>
                                 <th>Number</th>
@@ -27,25 +27,7 @@
                             </tr>
                         </thead>
                         <tbody> 
-                        @forelse ($product as $data)<tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $data->name }}</td>
-                            <td>{{ $data->quantity }} Pcs</td>
-                            <td>${{ $data->price }} USD</td>
-                            <td>
-                                <form action="{{ route('products.destroy', $data->id) }}" method="post">
-                                    @csrf 
-                                    <a class="btn btn-sm btn-primary" href="{{ route('products.edit', $data) }}">Edit</a>
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                        @empty
-                            <tr class="text-center">
-                                <td colspan="5"><i>There's no records, please create new record.</i></td>
-                            </tr>
-                        @endforelse
+                            
                         </tbody>
                     </table>
                 </div>
