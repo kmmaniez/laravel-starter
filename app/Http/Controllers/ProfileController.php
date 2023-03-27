ea91f792f68ab98726c0710351514105158cc174
kmmaniez <dzlabsid@gmail.com>
2023-03-27 21:03:51 +0700

    Add upload, save and delete image user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -22,6 +23,14 @@ class ProfileController extends Controller
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
+
+        if ($request->file('image')) {
+            Storage::delete('public/user/image/'. auth()->user()->image);
+            
+            $imageName = date('Ymd').'_'.date('His').'.'.$request->image->getClientOriginalExtension();
+            $request->file('image')->storeAs('public/user/image', $imageName);
+            $request->user()->image = $imageName;
+        }
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
